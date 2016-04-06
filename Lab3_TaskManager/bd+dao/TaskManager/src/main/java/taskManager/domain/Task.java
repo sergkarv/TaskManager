7b9a06c8f0fc17b35385ae5289d1200f7fdcd345
sergkarv <sergkarv@yandex.ru>
2016-04-06 07:32:54 +0400

    06.04.2016

---
+++ Lab3_TaskManager/bd+dao/TaskManager/src/main/java/taskManager/domain/Task.java
@@ -0,0 +1,86 @@
+package taskManager.domain;
+
+import taskManager.dao.Identified;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Сергей on 21.03.16.
+ */
+public class Task implements Identified<Integer> {
+    private Integer id = null;
+    private String name;
+    private String description;
+    private String contacts;
+    private Date date;
+    private boolean highPriority;
+    private Integer parentId;
+    private Integer userId;
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    protected void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(String contacts) {
+        this.contacts = contacts;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isHighPriority() {
+        return highPriority;
+    }
+
+    public void setHighPriority(boolean highPriority) {
+        this.highPriority = highPriority;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+    
+}
